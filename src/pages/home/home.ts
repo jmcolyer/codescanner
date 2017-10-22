e43603ce7cfbb4bf98e061c6bff23cb9eefc24d4
Jeff Colyer <jc72451@ex.kroger.com>
2017-10-22 14:42:22 -0400

    changed to add barcode scanner

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,55 +1,29 @@
 import { Component } from '@angular/core';
-import { AlertController } from 'ionic-angular';
-
-import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
-
+import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-	scanData: {};
-	encodeData: string ;
-	encodedData: {} ;
-	options: BarcodeScannerOptions;
-
-	constructor(public alertCtrl: AlertController, private barcodeScanner: BarcodeScanner) {
-
-	}
-
-	scan(){
-		this.options = {
-			prompt : "Scan your barcode "
-		}
-		this.barcodeScanner.scan(this.options).then((barcodeData) => {
-			console.log(barcodeData);
-			this.scanData = barcodeData;
-		}, (err) => {
-			console.log("Error occured : " + err);
-			this.presentAlert(JSON.stringify(err));
-		});         
-	}
-
-	encodeText(){
-		this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, this.encodeData).then((encodedData) => {
-			console.log(encodedData);
-			this.encodedData = encodedData;
-			//this.presentAlert(JSON.stringify(encodedData));
-		}, (err) => {
-			console.log("Error occured : " + err);
-			this.presentAlert(JSON.stringify(err));
-		});                 
-	}
-
-	presentAlert(message) {
-		const alert = this.alertCtrl.create({
-			title: 'Encoded data',
-			subTitle: message,
-			buttons: ['Dismiss']
-		});
-		alert.present();
-	}
-
+  qrData = null;
+  createdCode = null;
+  scannedCode = null;
+ 
+  constructor(private barcodeScanner: BarcodeScanner) { }
+ 
+  createCode() {
+    this.createdCode = this.qrData;
+  }
+ 
+  scanCode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.scannedCode = barcodeData.text;
+    }, (err) => {
+        console.log('Error: ', err);
+    });
+  }
+ 
 }
 
